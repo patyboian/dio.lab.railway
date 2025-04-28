5bd8ac3f5d04d8375fa602bb9cfdc1c7907d439d
Patricia Boian <patyboian86@gmail.com>
2025-04-28 14:15:46 -0300

    Envio dos arquivos do projeto.

---
+++ src/main/java/me/dio/domain/model/User.java
@@ -0,0 +1,75 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // campo gerado automaticamente
+    private Long id;
+
+    private String name;
+
+    @OneToOne(cascade = CascadeType.ALL) // relacionamento entre as tabelas, faz com que a tabela de usuario tenha um relacionamento com a tabela de conta
+    private Account account;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Card card;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER) // o eager siginifica que toda vez que buscar um usuario no banco, irá trazer a lista de features dele
+    private List<Feature> features;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<News> news;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public Card getCard() {
+        return card;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public List<Feature> getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(List<Feature> features) {
+        this.features = features;
+    }
+
+    public List<News> getNews() {
+        return news;
+    }
+
+    public void setNews(List<News> news) {
+        this.news = news;
+    }
+}
